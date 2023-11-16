b127c21936f4fd6db9d975367f435f795764da13
Pedro Mário Correia <pedro.mario@hotmail.com>
2023-11-16 16:28:29 -0300

    Configurando o Swagger

--- src/main/java/br/com/pedrodev/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/pedrodev/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,14 @@ package br.com.pedrodev.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Gestçao de Vagas",
+		description = "API responsável pela gestão de vagas",
+		version = "1"))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
